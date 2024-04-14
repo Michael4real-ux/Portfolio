5415276cf8358bd1c4a186502bafd235520213da
Michael <dammydeji.dd@gmail.com>
2024-04-14 14:17:35 +0100

    remove-image

--- src/app/components/HomeComponent/HomeComponent.js
+++ src/app/components/HomeComponent/HomeComponent.js
@@ -150,7 +150,7 @@ const  HomeComponent = () =>{
                   className=""
                   alt="Full-Stack Engineer"
                   ///ehizeex.png
-                  src="dami.jpeg"
+                  src="ehizeex.png"
                   decoding="async"
                   data-nimg="intrinsic"
                   width={0}
